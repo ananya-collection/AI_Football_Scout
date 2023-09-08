ebbdf0107abd4bfabff3fe2bcfaa6819d25fed4f
blaz2f <46502527+blaz2f@users.noreply.github.com>
2023-09-08 20:08:06 +1000

    fixed default application page start

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-let express= require('express');
-let app= express();
-let portNum= process.env.port ||3000;
+let express = require('express');
+let app = express();
+let port = process.env.port ||3000;
+const path = require('path');
 
-app.use(express.static(__dirname + '/'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public_html'), {index: "home.html"}));
 app.set('view engine', 'html');
 
- app.get('/', (req, res)=> {
-    console.log('hi');
-     res.render('./home.html');
- });
 
-app.listen(portNum, ()=>{
+
+
+
+app.listen(port, ()=>{
     //upon server start this logic will be fired
     console.log('server started');
 });
