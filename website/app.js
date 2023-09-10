a25050eefadff2654e776a032a79bd932e84490c
blaz2f <46502527+blaz2f@users.noreply.github.com>
2023-09-10 12:23:08 +1000

    refactored folder structure to MVC, was added auth tips

---
+++ website/app.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+let port = process.env.port ||3000;
+const path = require('path');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'views'), {index: "home.html"}));
+app.set('view engine', 'html');
+
+
+// Define routes and middleware here
+
+app.listen(port, ()=>{
+    //upon server start this logic will be fired
+    console.log('server started');
+});
---
+++ website/controller/authController.js
@@ -0,0 +1,2 @@
+//use bcrypt for hashing the password
+//use JsonWebToken to create the JWT after successful sign-in 
