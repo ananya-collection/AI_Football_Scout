c8335b3a788c0b4534c7905d58b1b966e5baeec5
Muhammad Sufian <Sufianjavidkbw@gmail.com>
2023-09-17 11:53:30 +0000

    render contact us page

--- website/controller/contactController.js
+++ website/controller/contactController.js
@@ -5,7 +5,7 @@ const Contact = require('../model/contact'); // Import your Contact model
 const ContactController = {
     saveContactUs: async (req, res) => {
         const { name, email, message } = req.body;
-        if (!title || !body || !pic) {
+        if (!name || !email || !message) {
           return res.status(422).json({ error: "Plase add all the fields" });
         }
         Contact.saveContactUs(req,res);
